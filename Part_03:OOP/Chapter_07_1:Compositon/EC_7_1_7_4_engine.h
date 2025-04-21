75a36a58be0483102defefc0fd603964970e2766
jinwoki2 <kemi07073@gmail.com>
2025-04-21 23:15:37 +0900

    7_4 fix norm

--- Part_03:OOP/Chapter_07_1:Compositon/EC_7_1_7_4_engine.h
+++ Part_03:OOP/Chapter_07_1:Compositon/EC_7_1_7_4_engine.h
@@ -12,11 +12,11 @@ struct engine_t	*engine_new(void);
 void			engine_ctor(struct engine_t *);
 
 // 소멸자
-void			engine_dtor(struct engine_t*);
+void			engine_dtor(struct engine_t *);
 
 // 행위 함수
-void			engine_turn_on(struct engine_t*);
-void			engine_turn_off(struct engine_t*);
-double			engine_get_temperature(struct engine_t*);
+void			engine_turn_on(struct engine_t *);
+void			engine_turn_off(struct engine_t *);
+double			engine_get_temperature(struct engine_t *);
 
 #endif
