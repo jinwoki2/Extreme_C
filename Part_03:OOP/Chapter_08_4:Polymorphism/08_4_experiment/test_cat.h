0993a5264c44fa1dcf91e105dbf158243831a1e2
jinwoki2 <kemi07073@gmail.com>
2025-05-10 20:22:21 +0900

    type_test

--- Part_03:OOP/Chapter_08_4:Polymorphism/08_4_experiment/test_cat.h
+++ Part_03:OOP/Chapter_08_4:Polymorphism/08_4_experiment/test_cat.h
@@ -1,10 +1,10 @@
 #ifndef TEST_CAT_H
 # define TEST_CAT_H
 
-struct s_cat;
+typedef struct s_cat t_cat; // 자료형이 이래도 된다!
 
 struct s_cat	*cat_new(void);
-void			cat_ctor(struct s_cat *cat, char *sound);
+void			cat_ctor(t_cat *cat, char *sound); // 첫 번째 매개변수에 대입해봄! 
 void			cat_sound(struct s_cat *cat);
 
 #endif
